1c9299b4c323cce64c5dc6d5bafe84f24bfab0e5
Myawmyaw <eco.villaraza@gmail.com>
2024-09-15 14:00:20 +0800

    fix: add delete on income seemore

--- BudgetTracker/Screens/Income/IncomeSeeMoreView.swift
+++ BudgetTracker/Screens/Income/IncomeSeeMoreView.swift
@@ -5,16 +5,19 @@
 //  Created by Jerico Villaraza on 9/7/24.
 //
 
+import SwiftData
 import SwiftUI
 
-/*
- TODO:
- - Add delete 
- 
- */
 struct IncomeSeeMoreView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @AppStorage("isIncomesEmpty") var isIncomesEmpty = true
+    
     var date: Date
     var incomes: [Income]
+    
+    @State private var isAlertPresented = false
+    @State private var incomesToDelete: [Income] = []
     
     var body: some View {
         List {
@@ -23,9 +26,45 @@ struct IncomeSeeMoreView: View {
                     IncomeItemView(income: income)
                 }
             }
+            .onDelete { offsets in
+                incomesToDelete = []
+                for index in offsets {
+                    let income = incomes[index]
+                    isAlertPresented = true
+                    incomesToDelete.append(income)
+                }
+            }
+            .alert(isPresented: $isAlertPresented){
+                Alert(
+                    title: Text("Are you sure you want to delete \(incomes.getPluralSuffix(singular: "this", plural: "these")) income\(incomes.getPluralSuffix(singular: "", plural: "s"))?"),
+                    message: Text("You cannot undo this action once done."),
+                    primaryButton: .destructive(Text("Delete"), action: actionDelete),
+                    secondaryButton: .cancel()
+                )
+            }
         }
         .navigationTitle(date.format(.dateOnly, descriptive: true))
     }
+    
+    
+    func actionDelete() {
+        for income in incomesToDelete {
+            modelContext.delete(income)
+        }
+        
+        do {
+            let fetchDescriptor = FetchDescriptor<Income>()
+            let fetchIncomes = try modelContext.fetch(fetchDescriptor)
+            
+            if fetchIncomes.isEmpty {
+                isIncomesEmpty = true
+            }
+            
+        } catch {
+            fatalError("Error deleting expense")
+        }
+    }
+
 }
 
 #Preview {
